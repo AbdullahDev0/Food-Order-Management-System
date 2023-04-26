e831ddc5768f45c09717cff51265ce1113fd1fd3
Abdullah Irfan <abdullahirfandev@gmail.com>
2023-04-26 19:24:39 +0500

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<p>APIs for FOMS! <a href="https://github.com/AbdullahDev0/Food-Order-Management-System"> GitHub </a> </p>';
   }
 }
